4e4e94803509116da3b3e8b0ef159f2636f77b2a
Srinivasan, Girish <Girish.Srinivasan@verint.com>
2022-06-15 16:42:48 +0530

    Try and get the git comments from a pull request.

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,36 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
+
+
+
+async function getLabels(prNumber: number, token: string): Promise<Set<String>> {
+  const octokit = github.getOctokit(token);
+  const context = github.context;
+
+  const prCommentsParams = {
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    issue_number: prNumber
+  }
+
+  const comments = await octokit.paginate(octokit.rest.issues.listComments, prCommentsParams)
+
+  const pullRequestComments = comments.filter(comment => comment !== null).map(comment => comment.body)
+  core.info(pullRequestComments.join('\n'));
+
+  return new Set<String>()
+}
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const token = core.getInput('github-token', { required: true }) || process.env.GITHUB_TOKEN;
+    const prNumber = parseInt(core.getInput('prNumber', { required: true }), 10);
+    if (!token)
+      throw new Error("No token specified");
 
-    core.setOutput('time', new Date().toTimeString())
+    await (getLabels(prNumber, token))
+
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
