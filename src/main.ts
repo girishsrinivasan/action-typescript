3916edf7c6e9495b4a5183a577df88403f91cf22
Srinivasan, Girish <Girish.Srinivasan@verint.com>
2022-06-15 18:13:46 +0530

    Debug

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import * as core from '@actions/core'
-//import * as github from '@actions/github'
-/*
+import * as github from '@actions/github'
+
 async function getLabels(prNumber: number, token: string): Promise<Set<String>> {
   const octokit = github.getOctokit(token)
   const context = github.context
@@ -17,19 +17,16 @@ async function getLabels(prNumber: number, token: string): Promise<Set<String>> 
   core.debug(txt)
   core.setOutput('log', txt)
   return new Set<String>(txt)
-}*/
+}
 
 async function run(): Promise<void> {
   try {
     core.debug('In run.')
-    core.setOutput('log', 'Some thing')
-    return
-    /*
     const token = core.getInput('token', {required: true}) || process.env.GITHUB_TOKEN
     const prNumber = parseInt(core.getInput('prNumber', {required: true}), 10)
     if (!token) throw new Error('No token specified')
 
-    await getLabels(prNumber, token)*/
+    await getLabels(prNumber, token)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
